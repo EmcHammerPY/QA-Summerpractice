24aa584582a7949dfc7d6ae077609d0aeed4642b
Yaroslav <zhurilkin01@gmail.com>
2021-06-16 10:04:46 +0300

    improvement programs

--- testsearch.py
+++ testsearch.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
+
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
+
 
 
 
@@ -10,7 +12,7 @@ word = "QA"
 
 
 driver = webdriver.Chrome("chromedriver")
-
+driver.implicitly_wait(10) # seconds
 driver.get("https://en.wikipedia.org/w/index.php?title=Special:UserLogin&returnto=Main+Page")
 
 driver.find_element_by_id("searchInput").send_keys(word)
@@ -18,15 +20,9 @@ driver.find_element_by_id("searchInput").send_keys(word)
 driver.find_element_by_id("searchButton").click()
 
 
-from selenium.webdriver.support.ui import Select
 
-select = Select(driver.find_element_by_id("content"))
-select.select_by_visible_text("QA")
 
-words = ['word', 'word1', 'word2']
-site = urllib.request.urlopen(link)
-for word in words:
-    if word in site:
-       print(word)
-    else:
-       print(word, "not found")
+for index in range(1,6):
+    path = f"//div[@id='mw-content-text']/ul/li[{index}]"
+    assert word in driver.find_element_by_xpath(path).text
+
