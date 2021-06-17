69abbed673d96667c097b17f662c88b87ef92493
Yaroslav <zhurilkin01@gmail.com>
2021-06-17 12:03:35 +0300

    added  assert

--- testloginfalse.py
+++ testloginfalse.py
@@ -1,37 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 
-
-username = "Testt1212"
+username = "Testt121"
 password = "1234567890QW"
-
+word = "Incorrect username or password entered. Please try again."
 
 driver = webdriver.Chrome("chromedriver")
-# перейти на страницу входа в github
+driver.implicitly_wait(10) # seconds
 driver.get("https://en.wikipedia.org/w/index.php?title=Special:UserLogin&returnto=Main+Page")
-
-# найти поле имени пользователя / электронной почты и отправить само имя пользователя в поле ввода
 driver.find_element_by_id("wpName1").send_keys(username)
-
-# найти поле ввода пароля и также вставить пароль
 driver.find_element_by_id("wpPassword1").send_keys(password)
-
-# нажмите кнопку входа в систему
 driver.find_element_by_id("wpLoginAttempt").click()
-# ждем завершения состояния готовности
-WebDriverWait(driver=driver, timeout=10).until(
-    lambda x: x.execute_script("return document.readyState === 'complete'")
-)
-error_message = "Incorrect username or password entered. Please try again."
+path = f"//div[@class = 'errorbox']"
+element = driver.find_element_by_xpath(path)
 
-# получаем ошибки (если есть)
-errors = driver.find_elements_by_class_name("errorbox")
+assert word == element.text
 
-# при необходимости распечатать ошибки
-# для e в ошибках:
-#     print(e.text)
-# если мы находим это сообщение об ошибке в составе error, значит вход не выполнен
-if any(error_message in e.text for e in errors):
-    print("[!] Login failed")
-else:
-    print("[+] Login successful")
+driver.quit()
+print("SUCCSES") 
--- testsearch.py
+++ testsearch.py
@@ -1,28 +1,17 @@
 from selenium import webdriver
-
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 
-
-
-
-
 word = "QA"
-
-
 
 driver = webdriver.Chrome("chromedriver")
 driver.implicitly_wait(10) # seconds
 driver.get("https://en.wikipedia.org/w/index.php?title=Special:UserLogin&returnto=Main+Page")
-
 driver.find_element_by_id("searchInput").send_keys(word)
-
 driver.find_element_by_id("searchButton").click()
-
-
-
-
-for index in range(1,6):
-    path = f"//div[@id='mw-content-text']/ul/li[{index}]"
-    assert word in driver.find_element_by_xpath(path).text
-
+path = f"//div[@id='mw-content-text']//li/a[@title]/.."
+elements=driver.find_elements_by_xpath(path)
+for index in range(5):    
+    assert word in elements[index].text
+driver.quit()
+print("SUCCSES")
